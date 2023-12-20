6d9f8425e3c5e1c28edf9aa7b114ec14fb41d454
ChatGTHB <cihat.kose@hotmail.com>
2023-12-20 16:55:41 +0100

    Added Ornek01.java for forLoop

---
+++ src/forLoop/Ornek01.java
@@ -0,0 +1,15 @@
+package forLoop;
+
+public class Ornek01 {
+    public static void main(String[] args) {
+
+/**
+ Javada 1'den 10'a kadar olan sayıları ekrana yazdıran
+ bir for döngüsü oluşturunuz.
+ */
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
