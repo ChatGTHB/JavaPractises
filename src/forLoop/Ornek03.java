24fcaff33ef317e559b26e538157d79ba8cc2596
ChatGTHB <cihat.kose@hotmail.com>
2023-12-20 16:55:54 +0100

    Added Ornek03.java for forLoop

---
+++ src/forLoop/Ornek03.java
@@ -0,0 +1,23 @@
+package forLoop;
+
+public class Ornek03 {
+
+    public static void main(String[] args) {
+
+        /**
+         Javada 1'den 10'a kadar olan tek sayıları ekrana yazdıran
+         ancak 7 sayısına gelindiğinde sonlanan bir for döngüsü oluşturunuz.
+         */
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (i == 7) {
+                break;
+            }
+            System.out.print(i + " ");
+        }
+
+    }
+}
