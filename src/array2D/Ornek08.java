7c84a007aabebafbe013af82384156c32fff3cb5
ChatGTHB <cihat.kose@hotmail.com>
2023-12-28 16:55:54 +0100

    Added some examples for 2D Arrays

---
+++ src/array2D/Ornek08.java
@@ -0,0 +1,26 @@
+package array2D;
+
+public class Ornek08 {
+
+    public static void main(String[] args) {
+
+        /**
+         2 satır ve sırasıyla 3 ve 6 sütundan oluşan bir 2 boyutlu dizi oluşturup,
+         bu diziyi FOR DÖNGÜSÜ KULLANMADAN rastgele değerlerle doldurun ve
+         elemanlarını FOR DÖNGÜSÜ KULLANARAK ekrana yazdırınız.
+         */
+
+        int[][] farkliBoyutlar = {
+                { (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)) },
+                { (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)),
+                        (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)), (int) (Math.random() * (100 + 1)) }
+        };
+
+        for (int i = 0; i < farkliBoyutlar.length; i++) {
+            for (int j = 0; j < farkliBoyutlar[i].length; j++) {
+                System.out.print(farkliBoyutlar[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/array2D/Ornek09.java
@@ -0,0 +1,28 @@
+package array2D;
+
+public class Ornek09 {
+
+    public static void main(String[] args) {
+
+        /**
+         2 satır ve sırasıyla 3 ve 6 sütundan oluşan bir 2 boyutlu dizi oluşturup,
+         bu diziyi FOR DÖNGÜSÜ KULLANARAK rastgele değerlerle doldurun ve elemanlarını ekrana yazdırınız.
+         */
+
+        int[][] farkliBoyutlar = new int[2][];
+        farkliBoyutlar[0] = new int[3]; // İlk satırda 3 sütun
+        farkliBoyutlar[1] = new int[6]; // İkinci satırda 6 sütun
+
+
+        System.out.println("****************");
+        for (int i = 0; i < farkliBoyutlar.length; i++) { // satır sayısı
+
+            for (int j = 0; j < farkliBoyutlar[i].length; j++) { // sütun sayısı
+                farkliBoyutlar[i][j] = (int) (Math.random() * (100 + 1));
+                System.out.print(farkliBoyutlar[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("****************");
+    }
+}
