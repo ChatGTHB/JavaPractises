02d94dcf65654a9a2130cf6221d662774de63868
ChatGTHB <cihat.kose@hotmail.com>
2024-02-15 17:10:11 +0100

    Edited

--- src/generalReview/ornek05/Ornek05.java
+++ src/generalReview/ornek05/Ornek05.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 public class Ornek05 {
     public static void main(String[] args) {
 
-        /*
-        Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden
-        ve girilen değerlerden çift ve 4'ün katları olan sayıları
-        toplayıp ekrana basan programı yazıyoruz.
-         */
-
         int sayi, toplam = 0;
 
         Scanner input = new Scanner(System.in);
@@ -22,6 +16,13 @@ public class Ornek05 {
             if (sayi % 4 == 0 && sayi != 0) {
                 toplam += sayi;
             }
+            /**
+             sayi != 0 ifadesi, girilen sayının sıfırdan farklı olup olmadığını kontrol eder.
+             Bu ifade, sıfırın toplama işlemine etkisini engellemek için kullanılır.
+             Eğer girilen sayı sıfıra eşitse, bu durumda toplam değişkenine eklenmez.
+             Bu ifadelerin kullanılması, programın sadece çift ve 4'ün katı olan sayıları
+             toplamasını sağlar ve sıfırın toplama işlemine etkisini engeller.
+             */
         } while (sayi % 2 == 0);
         System.out.println("Toplam = " + toplam);
     }
