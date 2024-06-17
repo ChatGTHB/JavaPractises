b5015647f7c419b7b8a48eaa090429ccf0cd32a4
ChatGTHB <cihat.kose@hotmail.com>
2024-06-17 19:40:58 +0200

    Added new classes

---
+++ src/_01_PrintPrintln.java
@@ -0,0 +1,31 @@
+public class _01_PrintPrintln {
+
+    public static void main(String[] args) {
+
+        // 1. Print ve Println
+
+        // Soru 1:
+        // Konsola "Merhaba Dünya" yazdırın.
+
+        System.out.println("Merhaba Dünya");
+        System.out.print("Merhaba Dünya");
+
+
+        //Soru 2:
+        //Konsola "Merhaba" ve "Dünya" kelimelerini aynı satırda yazdırın.
+
+        // "Merhaba" kelimesini konsola yazdırır ve satır başını yapmaz.
+        System.out.print("Merhaba ");
+        // "Dünya" kelimesini konsola yazdırır ve satır başı yapar
+        System.out.println("Dünya");
+
+
+        // Soru 3:
+        // Konsola "Merhaba" ve "Dünya" kelimelerini farklı satırlarda yazdırın.
+
+        // "Merhaba" kelimesini konsola yazdırır ve satır başını yapar.
+        System.out.println("Merhaba");
+        // "Dünya" kelimesini konsola yazdırır ve satır başı yapar
+        System.out.println("Dünya");
+    }
+}
