1ea9a49a35832343daa6e7b398060d0f31d0a79c
ChatGTHB <cihat.kose@hotmail.com>
2024-06-19 20:56:00 +0200

    Added new classes

---
+++ src/_04_DataTypes.java
@@ -0,0 +1,17 @@
+public class _04_DataTypes {
+    public static void main(String[] args) {
+
+//        Soru 1:
+//        Bir değişken tanımlayın ve değeri 10000003333333 olan bu değişkenin 1000 fazlasını ekrana yazdırın.
+
+        // Bir değişken tanımlayın ve değeri 10000003333333 olarak atayın.
+        long sayi = 10000003333333L;
+
+        // Değişkenin değerine 1000 ekleyin.
+        long yeniSayi = sayi + 1000;
+
+        // Değişkenin yeni değerini ekrana yazdırın.
+        System.out.println("yeniSayi = " + yeniSayi);
+
+    }
+}
