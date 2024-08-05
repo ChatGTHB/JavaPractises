f1d7a34b11fbe72a9fd959fd7d7e5e7f0df1420c
ChatGTHB <cihat.kose@hotmail.com>
2024-08-05 15:59:00 +0200

    Added new example

---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_07_Example/Config.java
@@ -0,0 +1,6 @@
+package _26_AccessModifiersAndNonAccessModifiers._07_Example;
+
+public class Config {
+    // public static final değişken: Sınıfa ait ve sabit bir değere sahiptir, değiştirilemez
+    public static final String VERSION = "1.0.0";
+}
---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_07_Example/TestConfig.java
@@ -0,0 +1,8 @@
+package _26_AccessModifiersAndNonAccessModifiers._07_Example;
+
+public class TestConfig {
+    public static void main(String[] args) {
+        // Static final değişken: Sınıf adı üzerinden erişilebilir
+        System.out.println("Config Version: " + Config.VERSION);
+    }
+}
