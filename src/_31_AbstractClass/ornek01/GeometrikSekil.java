d85c657c7a334c19699d5c319a2e402e75e97c5b
ChatGTHB <cihat.kose@hotmail.com>
2024-08-25 22:11:17 +0200

    Deleted some abstract class files

--- src/_31_AbstractClass/ornek01/GeometrikSekil.java
+++
@@ -1,8 +0,0 @@
-package _31_AbstractClass.ornek01;
-
-// GeometrikSekil abstract classı oluşturuldu.
-public abstract class GeometrikSekil {
-
-    // Alan hesaplama işlemini gerçekleştiren abstract bir method oluşturuldu.
-    public abstract double alanHesapla();
-}
--- src/_31_AbstractClass/ornek02/Hayvan.java
+++
@@ -1,9 +0,0 @@
-package _31_AbstractClass.ornek02;
-// Hayvan adında bir abstract class oluşturuldu
-
-public abstract class Hayvan {
-
-    // sesCikar adında bir abstract metot tanımlandı
-    public abstract void sesCikar();
-
-}
