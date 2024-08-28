e9b9e7696909205ff836686b2067dac4e9c370c1
ChatGTHB <cihat.kose@hotmail.com>
2024-08-28 16:56:27 +0200

    Added new abstract class files

---
+++ src/_32_AbstractClass/_04_Example/Bird.java
@@ -0,0 +1,9 @@
+package _32_AbstractClass._04_Example;
+
+public class Bird extends AnimalBase {
+    @Override
+    public void makeSound() {
+        System.out.println("Tweet!"); // Kuşun sesi
+    }
+}
+
---
+++ src/_32_AbstractClass/_04_Example/Cat.java
@@ -0,0 +1,9 @@
+package _32_AbstractClass._04_Example;
+
+public class Cat extends AnimalBase {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!"); // Kedinin sesi
+    }
+}
+
