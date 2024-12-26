94e01e4b61644cd55779cf2a61a157342259e2c3
Cihat Köse <cihat.kose@hotmail.com>
2024-12-27 00:17:04 +0100

    refactor: renamed _01_Example to _01_ScannerUserInput to improve clarity and keep task intact

--- src/_06_Scanner/_01_ScannerUserInput.java
+++ src/_06_Scanner/_01_ScannerUserInput.java
@@ -2,16 +2,16 @@ package _06_Scanner;
 
 import java.util.Scanner;
 
-public class _01_Example {
+public class _01_ScannerUserInput {
 
-    // **1. Task: Kullanıcıdan bir isim alarak ekrana ekrana
+    // 1. Task: Kullanıcıdan bir isim alarak ekrana
     // "Merhaba, {ad}!" şeklinde bir çıktı yazdıran bir program yazın.**
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("İsim giriniz: ");
-        // String isim2= input.next();
-        String isim= input.nextLine();
-        System.out.println("Merhaba, "+isim+"!");
+        // String isim2= input.next();  // Tek kelime alır
+        String isim = input.nextLine();  // Tüm satırı alır
+        System.out.println("Merhaba, " + isim + "!");
     }
 }
